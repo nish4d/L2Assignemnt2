09abe7c4b2ad82b077d0374a12c5c519274166ec
Nishad Islam <58546059+nish4d@users.noreply.github.com>
2025-05-26 23:53:25 +0600

    Update PostgreSQL_Assignment.sql

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,17 +1,17 @@
-
+-- rangers table create
 CREATE TABLE rangers (
     ranger_id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     region VARCHAR(50) NOT NULL
 );
 
-
+-- rangers data insert
 INSERT INTO rangers (name, region) VALUES
 ('Alice Green', 'Northern Hills'),
 ('Bob White', 'River Delta'),
 ('Carol King', 'Mountain Range');
 
-
+-- species table create
 CREATE TABLE species (
     species_id SERIAL PRIMARY KEY,
     common_name VARCHAR(50) NOT NULL,
@@ -21,13 +21,15 @@ CREATE TABLE species (
     CHECK (conservation_status IN ('Endangered', 'Vulnerable', 'Historic', 'Unknown'))
 );
 
-
+-- species data insert
 INSERT INTO species (common_name, scientific_name, discovery_date, conservation_status) VALUES
 ('Snow Leopard', 'Panthera uncia', '1775-01-01', 'Endangered'),
 ('Bengal Tiger', 'Panthera tigris tigris', '1758-01-01', 'Endangered'),
 ('Red Panda', 'Ailurus fulgens', '1825-01-01', 'Vulnerable'),
 ('Asiatic Elephant', 'Elephas maximus indicus', '1758-01-01', 'Endangered');
 
+
+-- sightings table create
 CREATE TABLE sightings (
     sighting_id SERIAL PRIMARY KEY,
     species_id INT REFERENCES species(species_id) ON DELETE CASCADE,
@@ -37,7 +39,7 @@ CREATE TABLE sightings (
     notes TEXT
 );
 
-
+-- sightings data insert
 INSERT INTO sightings (species_id, ranger_id, location, sighting_time, notes) VALUES
 (1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
 (2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
